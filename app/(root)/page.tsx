c961578c82e0fb08945c7e2ca8b676fe1c66c24f
Nikola Kenjic <nikola.nico89@gmail.com>
2025-06-19 00:00:06 +0200

    install shadcn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
     return (
@@ -5,6 +6,7 @@ export default function Home() {
             <h1 className="text-3xl font-bold underline">
                 Hello world!
             </h1>
+            <Button>Click me</Button>
         </>
     );
 }
