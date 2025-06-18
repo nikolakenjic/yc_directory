b6597d7a3afa4a7c5e40133a853b9191a27918ad
Nikola Kenjic <nikola.nico89@gmail.com>
2025-06-18 23:54:14 +0200

    add tailwindcss

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,10 @@
 
 export default function Home() {
     return (
-       <>
-           <h1>Home</h1>
-       </>
+        <>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </>
     );
 }
