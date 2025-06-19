1a88beaea4d4df092ed17144d7a815c09a0d9d00
Nikola Kenjic <nikola.nico89@gmail.com>
2025-06-19 20:39:36 +0200

    add styling to globals css

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,10 +3,12 @@ import {Button} from "@/components/ui/button";
 export default function Home() {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-            <Button>Click me</Button>
+            <section className='pink_container pattern'>
+                <h1 className="heading">
+                    Hello world!
+                </h1>
+                <Button className='mt-5' variant='outline'>Click me</Button>
+            </section>
         </>
     );
 }
