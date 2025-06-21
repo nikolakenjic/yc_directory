839451cd915149c02e8ddef20b96f87f23cfb4ae
Nikola Kenjic <nikola.nico89@gmail.com>
2025-06-21 21:53:18 +0200

    initial startup card

---
+++ components/StartupCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const StartupCard = ({post} : {post: StartupTypeCard}) => {
+    console.log(post);
+    return (
+        <li className='startup-card group'>
+            <div className='flex-between'>
+
+            </div>
+        </li>
+    );
+};
+
+export default StartupCard;
