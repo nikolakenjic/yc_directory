2a88a1630fa268557329ca5d7fe1cd719a03c22a
Nikola Kenjic <nikola.nico89@gmail.com>
2025-06-28 20:19:52 +0200

    get data by id

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import {client} from '@/sanity/lib/client'
+import {STARTUP_QUERY_DETAILS_BY_ID} from "@/sanity/lib/queries";
+import {notFound} from "next/navigation";
 
-const Page = async({params}: {params: Promise<{id: string}>}) => {
+export const experimental_ppr = true
+
+const Page = async ({params}: { params: Promise<{ id: string }> }) => {
     const {id} = await params
+    const post = await client.fetch(STARTUP_QUERY_DETAILS_BY_ID, {id})
 
     console.log(id)
+    if (!post) return notFound()
+
     return (
         <div>
-            <h1>Startup</h1>
+            <h1>{post.title}</h1>
         </div>
     );
 };
